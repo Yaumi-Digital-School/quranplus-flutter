a68f23d8935c1550b8610b9f2e8fcc45d9f42eb7
Mufti Ramdhani <muftiramdhani25@gmail.com>
2022-05-21 11:56:44 +0300

    add bottom navigation, and create main page

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:qurantafsir_flutter/pages/account_page.dart';
+import 'package:qurantafsir_flutter/pages/bookmark_page.dart';
+import 'package:qurantafsir_flutter/pages/home_page.dart';
+import 'package:qurantafsir_flutter/shared/constants/theme.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({ Key? key }) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    BookmarkPage(),
+    AccountPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex)
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: 'Bookmark',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: primary500,
+        selectedFontSize: 12,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
