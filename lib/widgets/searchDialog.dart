2f657f2eac16e4588ac20b885da8eff71e7e4760
Rhaka Febriandi <andirhakafebri@gmail.com>
2022-09-22 23:19:35 +0700

    separate file for the search dialog class & fix bug function ontap suggestion

--- lib/widgets/searchDialog.dart
+++ lib/widgets/searchDialog.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qurantafsir_flutter/shared/constants/theme.dart';
 
 class GeneralSearchDialog {
-  Future _searchDialog(BuildContext context, Widget widgetChild) {
+  Future searchDialog(BuildContext context, Widget widgetChild) {
     return showDialog(
       context: context,
       builder: (context) {
@@ -16,6 +16,99 @@ class GeneralSearchDialog {
               child: widgetChild),
         );
       },
+    );
+  }
+
+  Future searchDialoggeneral(BuildContext context, Function function) {
+    return searchDialog(
+      context,
+      Column(
+        children: [
+          const SizedBox(
+            height: 5.0,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 10,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 3 / 7,
+              child: const TextField(
+                enabled: true,
+                decoration: InputDecoration(hintText: '1', labelText: 'Surah'),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 25.0,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: const EdgeInsets.all(6.0),
+              primary: Colors.white,
+              onPrimary: primary500,
+              elevation: 2,
+              minimumSize: const Size.fromHeight(40),
+            ),
+            onPressed: () {
+              function;
+            },
+            child: Text('Search'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future searchDialogWithTabbar(BuildContext context, List<String> tabBar,
+      List<Widget> WidgetchildTabbarview) {
+    return searchDialog(
+      context,
+      DefaultTabController(
+        length: tabBar.length,
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height / 18,
+              decoration: BoxDecoration(
+                  color: neutral100,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const <BoxShadow>[
+                    BoxShadow(
+                      color: neutral300,
+                      blurRadius: 9.0,
+                      spreadRadius: 0.9,
+                    )
+                  ]),
+              child: Column(
+                children: [
+                  Container(
+                    child: TabBar(
+                        unselectedLabelColor: primary500,
+                        indicator: BoxDecoration(
+                            color: primary500,
+                            borderRadius: BorderRadius.circular(20)),
+                        tabs: tabBar
+                            .map((tabName) => Tab(child: Text(tabName)))
+                            .toList()),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height / 6,
+                child: TabBarView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: WidgetchildTabbarview,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
