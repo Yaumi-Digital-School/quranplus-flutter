f3ffc755047bf76ee62b0f8de061171cf3f01775
Yumna Pratista <yumnapratista24@Yumnas-MacBook-Air.local>
2022-10-28 17:51:53 +0700

    feat: add migration habit individu

---
+++ lib/shared/core/database/db_habit_daily_summary.dart
@@ -0,0 +1,10 @@
+class HabitDailySummaryTable {
+  static const String tableName = 'habit_daily_summary';
+  static const String columnID = 'id';
+  static const String target = 'target';
+  static const String pages = 'pages';
+  static const String date = 'date';
+  static const String targetUpdatedDate = 'target_updated_date';
+  static const String createdAt = 'created_at';
+  static const String updatedAt = 'updated_at';
+}
