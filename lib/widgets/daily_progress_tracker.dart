4bfec7d0b93b96d14423fad74910b4477df31e1e
Indra Septama <indraseptama28@gmail.com>
2022-11-06 10:41:03 +0000

    refactor: daily progress widget

--- lib/widgets/daily_progress_tracker.dart
+++ lib/widgets/daily_progress_tracker.dart
@@ -4,79 +4,65 @@ import 'package:qurantafsir_flutter/shared/constants/theme.dart';
 import 'package:intl/intl.dart';
 import 'package:qurantafsir_flutter/widgets/button.dart';
 
-class DailyProgresTracker extends StatelessWidget {
-  const DailyProgresTracker({
+class DailyProgressTracker extends StatelessWidget {
+  const DailyProgressTracker({
     Key? key,
     required this.target,
-    required this.dailyProgres,
-  });
+    required this.dailyProgress,
+  }) : super(key: key);
 
   final int target;
-  final double dailyProgres;
+  final double dailyProgress;
   @override
   Widget build(BuildContext context) {
     DateTime now = DateTime.now();
     String formattedDate = DateFormat('EEEE, d MMMM yyyy').format(now);
 
-    int dailyProgressToInt = dailyProgres.round();
+    int dailyProgressToInt = dailyProgress.floor();
 
-    return Card(
-      color: Colors.white,
-      elevation: 1.2,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-      ),
-      child: SizedBox(
-        width: double.infinity,
-        height: 176,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(12, 17, 12, 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "Today's Progress",
-                style: TextStyle(fontSize: 17, fontWeight: semiBold),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(
-                formattedDate,
-                style: TextStyle(
-                    fontSize: 12, color: Colors.grey, fontWeight: medium),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: LinearPercentIndicator(
-                  animation: true,
-                  lineHeight: 20.0,
-                  animationDuration: 1000,
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  percent: dailyProgres / target,
-                  barRadius: const Radius.circular(9),
-                  progressColor: darkGreen,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 5),
-                alignment: Alignment.centerRight,
-                child: Text(
-                  target > 1
-                      ? '$dailyProgressToInt / $target pages'
-                      : '$dailyProgressToInt / $target page',
-                  style: TextStyle(
-                      fontSize: 12, fontWeight: regular, color: neutral600),
-                ),
-              ),
-              ButtonSecondary(label: 'See Detail', onTap: () {})
-            ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Today's Progress",
+          style: TextStyle(fontSize: 17, fontWeight: semiBold),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(
+          formattedDate,
+          style:
+              TextStyle(fontSize: 12, color: Colors.grey, fontWeight: medium),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: LinearPercentIndicator(
+            animation: true,
+            lineHeight: 16.0,
+            animationDuration: 1000,
+            padding: const EdgeInsets.symmetric(horizontal: 0),
+            percent: dailyProgress / target,
+            barRadius: const Radius.circular(8),
+            progressColor: darkGreen,
+            backgroundColor: neutral300,
           ),
         ),
-      ),
+        const SizedBox(height: 8),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Text(
+            target > 1
+                ? '$dailyProgressToInt / $target pages'
+                : '$dailyProgressToInt / $target page',
+            style:
+                TextStyle(fontSize: 12, fontWeight: regular, color: neutral600),
+          ),
+        ),
+      ],
     );
   }
 }
