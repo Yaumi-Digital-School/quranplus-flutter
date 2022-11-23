f328fd84529ce8649309ab95b0e46ae419033261
Yumna Pratista <yumnanaruto@gmail.com>
2022-11-23 19:57:56 +0700

    feat: primary button component

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 import '../shared/constants/theme.dart';
 
+enum ButtonSize {
+  regular,
+  small,
+}
+
 class ButtonSecondary extends StatelessWidget {
   const ButtonSecondary({
     required this.label,
@@ -88,5 +93,46 @@ class ButtonNeutral extends StatelessWidget {
           onPressed: onTap,
           child: Text(label),
         ));
+  }
+}
+
+class ButtonPrimary extends StatelessWidget {
+  const ButtonPrimary({
+    required this.label,
+    required this.onTap,
+    this.size = ButtonSize.regular,
+  });
+
+  final String label;
+  final Function()? onTap;
+  final ButtonSize size;
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    final double width = size == ButtonSize.regular ? double.infinity : 100;
+    final double labelFontSzie = size == ButtonSize.regular ? 14 : 12;
+
+    return SizedBox(
+      width: width,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          padding: const EdgeInsets.all(6.0),
+          primary: darkGreen,
+          onPrimary: Colors.white,
+          elevation: 0,
+          minimumSize: const Size.fromHeight(40),
+        ),
+        onPressed: onTap,
+        child: Text(
+          label,
+          style: TextStyle(fontSize: labelFontSzie),
+        ),
+      ),
+    );
   }
 }
