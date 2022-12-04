b46dacc27aa7f9038c9041b78d15b8865941b4c1
Yumna Pratista <yumnanaruto@gmail.com>
2022-12-04 12:44:03 +0000

    feat: add api for habit sync

--- lib/shared/core/providers.dart
+++ lib/shared/core/providers.dart
@@ -1,5 +1,6 @@
 import 'package:qurantafsir_flutter/pages/settings_page/settings_page_state_notifier.dart';
 import 'package:qurantafsir_flutter/shared/core/apis/bookmark_api.dart';
+import 'package:qurantafsir_flutter/shared/core/apis/habit_api.dart';
 import 'package:qurantafsir_flutter/shared/core/apis/user_api.dart';
 import 'package:qurantafsir_flutter/shared/core/env.dart';
 import 'package:qurantafsir_flutter/shared/core/services/authentication_service.dart';
@@ -30,6 +31,13 @@ final Provider<SharedPreferenceService> sharedPreferenceServiceProvider =
 final Provider<BookmarkApi> bookmarkApiProvider = Provider<BookmarkApi>((ref) {
   final DioService dioService = ref.watch(dioServiceProvider);
   return BookmarkApi(
+    dioService.getDioWithAccessToken(),
+  );
+});
+
+final Provider<HabitApi> habitApiProvider = Provider<HabitApi>((ref) {
+  final DioService dioService = ref.watch(dioServiceProvider);
+  return HabitApi(
     dioService.getDioWithAccessToken(),
   );
 });
