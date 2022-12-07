ef097e1d7857e182f53f8a9bb700bc6819df97d1
rhaka17 <andirhakafebri@gmail.com>
2022-12-07 17:19:33 +0700

    fix: increment count in same page

--- lib/pages/surat_page_v3/surat_page_state_notifier.dart
+++ lib/pages/surat_page_v3/surat_page_state_notifier.dart
@@ -222,26 +222,33 @@ class SuratPageStateNotifier extends BaseStateNotifier<SuratPageState> {
   }
 
   void changePageOnRecording(int page, int startPage) {
+    print(startPage);
     if (recordedPagesList.isEmpty) {
       if (page > startPage) {
         recordedPagesAsRead.value += 1;
         recordedPagesList.add(startPage);
+        print("halo");
       }
+      print("hai");
 
       return;
     }
 
-    if (recordedPagesList.contains(page)) {
+    if (recordedPagesList.contains(page - 1)) {
+      print("hola");
       return;
     }
 
     final int firstReadPage = recordedPagesList[0];
-    if (page < firstReadPage) {
+    if (page <= firstReadPage) {
+      print("loha");
       return;
     }
 
+    print("selamat");
     recordedPagesList.add(page - 1);
     recordedPagesAsRead.value += 1;
+    print(recordedPagesList);
   }
 
   bool isAyahFavorited(int ayahID) {
