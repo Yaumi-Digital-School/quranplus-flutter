340e2ad39b16d4d27ae33c900d18d88fd8fbcbaa
indraseptama <indraseptama28@gmail.com>
2023-05-31 00:48:44 +0700

    add data design system

---
+++ lib/shared/constants/qp_theme_data.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:qurantafsir_flutter/shared/constants/qp_colors.dart';
+
+enum QPThemeMode {
+  dark,
+  light,
+  brown,
+}
+
+class QPThemeData {
+  static ThemeData get lightThemeData {
+    return ThemeData(
+      scaffoldBackgroundColor: QPColors.whiteFair,
+      dividerColor: QPColors.whiteRoot,
+      colorScheme: const ColorScheme.light().copyWith(
+        primary: QPColors.blackHeavy,
+        primaryContainer: QPColors.whiteMassive,
+        secondaryContainer: QPColors.whiteHeavy,
+      ),
+    );
+  }
+
+  static ThemeData get darkThemeData {
+    return ThemeData(
+      scaffoldBackgroundColor: QPColors.darkModeMassive,
+      dividerColor: QPColors.darkModeFair,
+      colorScheme: const ColorScheme.light().copyWith(
+        primary: QPColors.whiteFair,
+        primaryContainer: QPColors.darkModeHeavy,
+        secondaryContainer: QPColors.darkModeFair,
+      ),
+    );
+  }
+
+  static ThemeData get brownThemeData {
+    return ThemeData(
+      scaffoldBackgroundColor: QPColors.brownModeRoot,
+      dividerColor: QPColors.brownModeFair,
+      colorScheme: const ColorScheme.light().copyWith(
+        primary: QPColors.brownModeMassive,
+        primaryContainer: QPColors.brownModeFair,
+        secondaryContainer: QPColors.brownModeRoot,
+      ),
+    );
+  }
+}
