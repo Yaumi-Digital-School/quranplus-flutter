1344f27ae005dde9bc4b99cbfa855a64d8a8d2c4
indraseptama <indraseptama28@gmail.com>
2023-06-04 21:14:24 +0700

    move asset to svg folder

--- lib/shared/constants/icon.dart
+++ lib/shared/constants/icon.dart
@@ -6,8 +6,8 @@ class IconPath {
   static String iconCollaborate = 'images/icon_collaborate.png';
   static String iconGoogle = 'images/icon_google.png';
   static String iconApple = 'images/icon_apple.png';
-  static String iconAccount = 'images/icon-account.svg';
-  static String iconLogout = 'images/logout-icon.svg';
+  static String iconAccount = 'images/svg/icon-account.svg';
+  static String iconLogout = 'images/svg/logout-icon.svg';
   static String iconSync = 'images/icon_sync.png';
   static String iconUpdateNow = 'images/icon_update_now.png';
   static String iconFavoriteInactive = 'images/icon_favorite_inactive.png';
@@ -19,5 +19,5 @@ class IconPath {
   static String iconLeaveGroup = 'images/icon_leave_group.png';
   static String iconInviteMember = 'images/icon_invite_member.png';
   static String iconEditSquare = 'images/icon_edit_square.png';
-  static String iconTheme = "images/theme-icon.svg";
+  static String iconTheme = "images/svg/theme-icon.svg";
 }
