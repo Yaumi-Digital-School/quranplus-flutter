5fe8ff55f8beeb4559f3f2dcda16d45922dbca8d
pratista24 <36220334+yumnapratista24@users.noreply.github.com>
2023-06-18 14:27:35 +0700

    feat: implement select theme on reading page (#15)
    * feat: implement select theme on reading page
    
    * refactor: add delay

---
+++ lib/shared/core/models/theme_option_color_param.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class ThemeOptionColorParam {
+  ThemeOptionColorParam({
+    required this.firstColor,
+    required this.secondColor,
+    required this.thirdColor,
+  });
+
+  final Color firstColor;
+  final Color secondColor;
+  final Color thirdColor;
+}
