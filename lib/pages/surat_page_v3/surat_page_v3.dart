adc8405315d40fdc612dd57d548ddb69d26cadfd
Rhaka febriandi <andirhakafebri@gmail.com>
2023-06-18 15:47:05 +0700

    fix : fontSize on Basmalah

--- lib/pages/surat_page_v3/surat_page_v3.dart
+++ lib/pages/surat_page_v3/surat_page_v3.dart
@@ -580,6 +580,7 @@ class _SuratPageV3State extends State<SuratPageV3> {
       }
 
       return _buildBasmalah(
+        orientation: orientation,
         isInFullPage: true,
       );
     }
@@ -612,8 +613,7 @@ class _SuratPageV3State extends State<SuratPageV3> {
 
     if (orientation == Orientation.landscape) {
       return SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 30, right: 30),
+        child: Center(
           child: AutoSizeText(
             text,
             style: TextStyle(
@@ -821,7 +821,7 @@ class _SuratPageV3State extends State<SuratPageV3> {
         key: key,
         child: Column(
           children: <Widget>[
-            if (useBasmalahBeforeAyah) _buildBasmalah(),
+            if (useBasmalahBeforeAyah) _buildBasmalah(orientation: orientation),
             GestureDetector(
               onLongPress: () {
                 GeneralBottomSheet().showGeneralBottomSheet(
@@ -1032,8 +1032,17 @@ class _SuratPageV3State extends State<SuratPageV3> {
 
   Widget _buildBasmalah({
     bool isInFullPage = false,
+    required Orientation orientation,
   }) {
     if (isInFullPage) {
+      if (orientation == Orientation.landscape) {
+        return Image.asset(
+          'images/bismillah_v2.png',
+          width: 300,
+          color: Theme.of(context).colorScheme.primary,
+        );
+      }
+
       return Image.asset(
         'images/bismillah_v2.png',
         width: 170,
