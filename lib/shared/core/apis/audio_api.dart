f0091d1580e252d04a964c88e2fb0cc13e9afa3a
indraseptama <indraseptama28@gmail.com>
2023-07-30 14:51:48 +0700

    add api

--- lib/shared/core/apis/audio_api.dart
+++ lib/shared/core/apis/audio_api.dart
@@ -1,5 +1,7 @@
 import 'package:retrofit/retrofit.dart';
 import 'package:dio/dio.dart';
+
+import 'model/audio.dart';
 part 'audio_api.g.dart';
 
 @RestApi()
@@ -7,7 +9,8 @@ abstract class AudioApi {
   factory AudioApi(Dio dio, {String baseUrl}) = _AudioApi;
 
   @GET('/api/recitations/{reciter_id}/by_ayah/{surah_id}/{ayah_number}')
-  Future<HttpResponse> getAudioForSpecificReciterAndAyah({
+  Future<HttpResponse<AudioSpecificAyahResponse>>
+      getAudioForSpecificReciterAndAyah({
     @Path('reciter_id') required int reciterId,
     @Path('surah_id') required int surahId,
     @Path('ayah_number') required int ayahNumber,
