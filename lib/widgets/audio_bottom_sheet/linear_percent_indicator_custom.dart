fbf54f2fd8b5d025eee2a822a5a92a74b087ed5f
Yumna Pratista <yumnanaruto@gmail.com>
2023-08-02 20:44:13 +0700

    refactor: add seek function

--- lib/widgets/audio_bottom_sheet/linear_percent_indicator_custom.dart
+++ lib/widgets/audio_bottom_sheet/linear_percent_indicator_custom.dart
@@ -1,6 +1,7 @@
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:qurantafsir_flutter/shared/constants/qp_text_style.dart';
 import 'package:qurantafsir_flutter/shared/core/providers/audio_provider.dart';
 
@@ -13,8 +14,12 @@ class LinearPercentIndicatorCustom extends ConsumerWidget {
         ref.watch(currentDurationProvider);
     final AsyncValue<Duration?> totalDuration =
         ref.watch(totalDurationProvider);
+    final AudioPlayer _player = ref.read(audioPlayerProvider);
 
     return ProgressBar(
+      onSeek: ((Duration value) {
+        _player.seek(value);
+      }),
       timeLabelTextStyle: QPTextStyle.getDescription2Regular(context),
       progress: currentDuration.when(
         data: (data) => data,
